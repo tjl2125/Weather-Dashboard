8878259d7706cf2a071aeb68c922583c04dd2587
Taylor Leong <tjl2125@alum.barnard.edu>
2021-03-12 17:25:22 -0500

    html structure

---
+++ script.js
@@ -0,0 +1,13 @@
+var forecastUrl = "https://api.openweathermap.org/data/2.5/weather"
+var keyAPI = "9cf609413c8a8ba8656e92d51411f9af"
+
+
+function weatherForecast (forecastUrl){
+    fetch (forecastUrl)
+    
+
+
+
+
+
+}
